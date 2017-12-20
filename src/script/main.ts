224394568e998c8f9cd801972f286bce4ff9e456
codeartistic.ninja <the@codeartistic.ninja>
2017-12-20 15:03:57 +0100

    sound and music

--- src/script/main.ts
+++ src/script/main.ts
@@ -1,3 +1,7 @@
+import Sound from "./_classes/utils/Sound";
+import MediaPlayer from "./_classes/utils/MediaPlayer";
+import StorageFile from "./_classes/utils/StorageFile";
+
 // import MyGame from "./_classes/MyGame";
 
 /**
@@ -5,11 +9,17 @@
  * Main script
  */
 // var game: MyGame;
+let slideSnd = new Sound("./sound/agfa.wav");
+let music = new MediaPlayer();
+let prefs = new StorageFile("/prefs.json");
+let ttm = 2;
 
 function init() {
   if (location.hash.length > 1) location.replace("./");
   // game = (<any>window)["game"] = new MyGame("#game");
   // game.debug = location.search.indexOf("debug") !== -1;
+  _initAudio();
+  slideSnd.oneInstance = true;
   let slideshow = document.querySelector("#slideshow");
   window.addEventListener("resize", () => {
     resize();
@@ -33,6 +43,10 @@ function init() {
 
 function changeSlide(e?: Event, reverse = false) {
   flash();
+  slideSnd.play();
+  if (!ttm--) setTimeout(() => {
+    music.play("./music/Thinking Music.mp3", true);
+  }, 1024);
   if (e && (<HTMLElement>e.target).tagName === "A") return;
   let vignette = <HTMLElement>document.querySelector(".vignette");
   let show = false;
@@ -89,6 +103,23 @@ function resize() {
     document.body.classList.remove("wide");
   }
 }
+
+function _initAudio() {
+  var vol = 2;
+  for (var channel of ["sfx", "music", "ambiance"]) {
+    prefs.set(channel + ".enabled", true, true);
+    prefs.set(channel + ".volume", vol /= 2, true);
+    prefs.onSet(channel, applySoundPrefs);
+  }
+  applySoundPrefs();
+}
+function applySoundPrefs() {
+  music.enabled = prefs.get("music.enabled");
+  music.volume = prefs.get("music.volume");
+  Sound.enabled = prefs.get("sfx.enabled");
+  Sound.volume = prefs.get("sfx.volume");
+}
+
 
 if (location.search === "?nojs") {
   let tags = document.getElementsByTagName("noscript");
